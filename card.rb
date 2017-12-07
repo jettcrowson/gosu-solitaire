028e71afb050e51b2c8956fc2e531020a822da2e
Jett Crowson <jettcrowson@gmail.com>
2017-12-07 15:54:17 -0700

    Add a lot

--- card.rb
+++ card.rb
@@ -1,5 +1,7 @@
 #To be made 52 different times
 
+require "./z_order"
+
 class Card
 
     #These should not ever be changed, so we can just put a reader
@@ -18,14 +20,26 @@ class Card
         #Set the color according to suit
         @color = suit == "Hearts" || suit == "Diamonds" ? "red" : "black"
 
-        @x = 0
+        @x = 100
         @y = 0
+
+        @base_card_image = Gosu::Image.new("images/card.png", tileable: true)
     end
 
     #For changing the position of the card
     def set_pos(set_x, set_y)
         self.x = set_x
         self.y = set_y
+    end
+
+    def draw
+        @base_card_image.draw(x, y, ZOrder::Card)
+        puts x
+    end
+
+    #Instead of printing an object address, it will print somthing like "2 of Spades"
+    def to_s
+        return "#{val} of #{suit}"
     end
 
 
